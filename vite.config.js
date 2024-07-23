23fee040ca100e51bcdaf10a822ed874453d847e
TehilaEtinger <tehilla4468@gmail.com>
2024-07-23 11:59:54 +0300

    visalized

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: './dist/bundle-visualizer.html',
+      open: true, // Automatically opens the visualizer report after build
+    })
+  ],
   build: {
     rollupOptions: {
       output: {
