0f4bcef661cc8f731fd1e929759bb78d92884e9f
TehilaEtinger <tehilla4468@gmail.com>
2024-07-23 11:45:46 +0300

    manual chunking and optimized build

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense, lazy } from 'react';
 import { useDispatch } from 'react-redux';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { query, collection, onSnapshot } from 'firebase/firestore';
 import db from './firebase';
-import HomePage from './HomePage';
 import NavBar from './NavBar';
-import Payment from './PaymentPage';
-import ViewInventory from './InformationTablesPage'
-import UpdateInventory from './UpdateInventoryPage'
-import PayToSupPage from './PayToSupPage';
-import UpdatedProducts from './UpdateProductsPage';
-import UserManagement from './UserManagementPage';
-import StatisticsPage from './StatisticsPage';
-import DealsPage from './DealsPage';
-import NotFound from './NotFound';
+
+// Use React.lazy for dynamic imports
+const HomePage = lazy(() => import('./HomePage'));
+const Payment = lazy(() => import('./PaymentPage'));
+const ViewInventory = lazy(() => import('./InformationTablesPage'));
+const UpdateInventory = lazy(() => import('./UpdateInventoryPage'));
+const PayToSupPage = lazy(() => import('./PayToSupPage'));
+const UpdatedProducts = lazy(() => import('./UpdateProductsPage'));
+const UserManagement = lazy(() => import('./UserManagementPage'));
+const StatisticsPage = lazy(() => import('./StatisticsPage'));
+const DealsPage = lazy(() => import('./DealsPage'));
+const NotFound = lazy(() => import('./NotFound'));
 
 function App({ userRole }) {
   const dispatch = useDispatch();
@@ -53,27 +55,24 @@ function App({ userRole }) {
     return userRole === requiredRole ? true : false;
   };
 
-
   return (
-    <div style={{width:'80vw' ,marginLeft:'10vw', marginRight:'10vw' }}>
+    <div style={{ width: '80vw', marginLeft: '10vw', marginRight: '10vw' }}>
       <NavBar />
-
-      <Routes basename="/">
-        <Route path="/" element={<HomePage />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/view" element={<ViewInventory />} />
-            <Route path="/update" element={<UpdateInventory />} />
-            <Route path="/updateprod" element={<UpdatedProducts />} />
-            <Route path="/PayToSupPage" element={<PayToSupPage />} />
-            <Route path="/users" element={<UserManagement />} />
-            <Route path="/statistics" element={<StatisticsPage />} />
-            <Route path="/deals" element={<DealsPage />} />
-
-       
-
-        
-      </Routes>
+      {/* Use Suspense to wrap lazy-loaded routes */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes basename="/">
+          <Route path="/" element={<HomePage />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/view" element={<ViewInventory />} />
+          <Route path="/update" element={<UpdateInventory />} />
+          <Route path="/updateprod" element={<UpdatedProducts />} />
+          <Route path="/PayToSupPage" element={<PayToSupPage />} />
+          <Route path="/users" element={<UserManagement />} />
+          <Route path="/statistics" element={<StatisticsPage />} />
+          <Route path="/deals" element={<DealsPage />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
